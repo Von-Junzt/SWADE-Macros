fc12ed0d3eba30567d1de17ab80d1462da784255
aushilfsalien <nils.prior@outlook.de>
2025-02-21 21:31:16 +0100

    added reloading sounds

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ Hooks.on('BRSW-RollItem', async (br_message, html) => {
         await item.setFlag('vjp-macros', 'originalShots', item.system.currentShots);
     }
 });
+
+Hooks.on('swadeReloadWeapon', async (item, reloaded) => {
+    ui.notifications.info(item?.parent?.name + 'reloaded his weapon: ' + item.name);
+    console.log(item);
+    // Create audio object
+    const sfxData = item.getFlag('swim', 'config');
+    const sfxToPlay = sfxData.reloadSFX || "";
+    if (sfxToPlay === "") {
+        ui.notifications.warn('No reload sound set for this weapon.');
+    }
+    // Play the sound
+    const activeUserIds = game.users.filter(user => user.active).map(user => user.id);
+    new Sequence()
+        .sound()
+        .file(sfxToPlay)
+        .forUsers(activeUserIds)
+        .play();
+});
