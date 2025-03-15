356e2c1a86e7de2db15bb388bb5438ecadbd3ac8
aushilfsalien <nils.prior@outlook.de>
2025-03-16 00:11:42 +0100

    added gas vent system

--- main.js
+++ main.js
@@ -25,48 +25,12 @@ Hooks.once('ready', async () => {
 // add a visual effect for the ducking effect
 Hooks.on("updateToken", toggleDuckingEffect);
 
-Hooks.on('getItemSheetHeaderButtons', function (sheet, buttons) {
-    // Only modify sheets for items of type "weapon"
-    if (sheet.document.type !== 'weapon') return;
-
-    buttons.unshift({
-        class: 'manage-enhancements',
-        label: 'Enhancements',
-        icon: 'fas fa-tools',
-        onclick: () => {
-            // Check if a dialog is already open for this item
-            if (openEnhancementDialogs.has(sheet.document.id)) {
-                const dialog = openEnhancementDialogs.get(sheet.document.id);
-                // Check if the dialog's element exists before calling bringToFront()
-                if (dialog.element) {
-                    dialog.bringToFront();
-                } else {
-                    // If the dialog is closed or its element is missing, re-render it
-                    dialog.render({ force: true });
-                }
-            } else {
-                // Create a new dialog and track it
-                const dialog = new EnhancementsDialog(sheet.document);
-                dialog.render({ force: true });
-                openEnhancementDialogs.set(sheet.document.id, dialog);
-
-                // Store the original close function.
-                const originalClose = dialog.close.bind(dialog);
-                // Override the close method to ensure proper cleanup.
-                dialog.options.window.close = async () => {
-                    openEnhancementDialogs.delete(sheet.document.id);
-                    return await originalClose();
-                };
-            }
-        }
-    });
-});
-
 // Register a hook that fires when item sheets are rendered
 Hooks.on("renderItemSheet", (app, html, data) => {
-    // Check if this is a weapon
+    // Check if this is a weapon and also if the weapontype is in the animationData
     const item = app.object;
-    if (item.type === "weapon") {
+    const weaponType = getWeaponType(item);
+    if (item.type === "weapon" && weaponType) {
         // Check if we already have a dialog open for this item
         if (!openEnhancementDialogs.has(item.id)) {
             // Small delay to ensure the sheet is fully rendered and positioned
@@ -104,10 +68,7 @@ Hooks.on('BRSW-RollItem', async (br_message, html) => {
         // set the original shots count
         await item.setFlag('vjpmacros', 'originalShots', item.system.currentShots);
         // determine weapon type by name
-        const weaponName = item.system?.category.toLowerCase();
-        const weaponType = Object.keys(animationData)
-            .sort((a, b) => b.length - a.length)  // Sort by length descending
-            .find(type => weaponName.includes(type)) || undefined;
+        const weaponType = getWeaponType(item);
         // if we have a valid weapon type, we can play the animation
         if (weaponType) {
             await repeatingWeapon(br_message, weaponType);
@@ -128,3 +89,11 @@ Hooks.on('swadeReloadWeapon', async (item, reloaded) => {
         await playWeaponReloadSfx(item);
     }
 });
+
+function getWeaponType(item) {
+    const weaponCategory = item.system?.category?.toLowerCase() || "";
+    const weaponType = Object.keys(animationData)
+        .sort((a, b) => b.length - a.length)
+        .find(type => weaponCategory.includes(type)) || undefined;
+    return weaponType;
+}
