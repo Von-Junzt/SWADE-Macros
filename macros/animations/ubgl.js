3ba7461c31d5c0cd9039d9d74031e4b0df91b096
aushilfsalien <nils.prior@outlook.de>
2025-03-20 22:36:02 +0100

    added underbarrel grenade launcher mod

--- macros/animations/ubgl.js
+++ macros/animations/ubgl.js
@@ -3,6 +3,16 @@ const lastTemplate = canvas.templates.placeables[canvas.templates.placeables.len
 const sfxToPlay = game.vjpmacros.sfxData["ubgl"].fireSFX;
 const animationToPlay = "modules/vjpmacros/assets/gfx/weapons/grenades/m203_grenade_green.webp";
 const activeUserIds = game.users.filter(user => user.active).map(user => user.id);
+const ray = new Ray(sourceToken.center, lastTemplate)
+const rotation = (ray.angle * 180 / Math.PI) - 90;
+const muzzleFlashOffsetDistance = canvas.grid.size * 0.6;  // Adjust as needed
+const muzzleFlashPoint = {
+    x: sourceToken.center.x + Math.cos(ray.angle) * muzzleFlashOffsetDistance,
+    y: sourceToken.center.y + Math.sin(ray.angle) * muzzleFlashOffsetDistance,
+};
+
+await sourceToken.document.update({rotation: rotation}, {animate: false});
+
 
 await new Sequence()
     .sound()
